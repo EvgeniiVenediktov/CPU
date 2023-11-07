46896ce0efe6e18d2f8ed04c6fc0c934fb8314ac
EvgeniiVenediktov <evv13@pitt.edu>
2023-11-07 17:20:04 -0500

    add cpu.py

---
+++ cpu.py
@@ -0,0 +1,30 @@
+# CPU simulation
+from memory import Memory
+
+
+### Create instances of all modules: ###
+
+# Monitor - TODO
+
+# Input Decoder - TODO - 🛠️ in progress
+
+# CDB - TODO
+
+# Register Module:
+## Register Alias Table - TODO
+## Architecture Register Files - TODO
+
+# Reorder Module:
+## Reorder Buffer - TODO
+## Renaming Module - TODO
+
+# Execution Module
+## Functioanal Modules (Adders, Multipliers) - TODO
+## Reservation Stations - TODO
+
+# Memory Module
+## Address Resolver - TODO
+## Load/Store Buffers - TODO
+## Memory - DONE ✔️
+mem_init_file = ""
+hard_memory = Memory(mem_init_file)
