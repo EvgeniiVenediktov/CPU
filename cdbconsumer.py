35fa16197926a6d73595dbb51835df4502f8ed45
EvgeniiVenediktov <evv13@pitt.edu>
2023-11-11 14:51:08 -0500

    start reservation stations

--- cdbconsumer.py
+++ cdbconsumer.py
@@ -1,9 +1,9 @@
-# CDB Receiver super-class
+# CDB Consumer super-class
 from cdb import CentralDataBus, NameAndValue
 
-class CDBReceiver():
+class CDBConsumer():
     """
-        CDB Receiver is a superclass.
+        CDB Consumer is a superclass.
         It's heirs are able to listen to CDB (Central Data Bus) 
         and check for the variables values they await at the moment.
 
@@ -11,6 +11,10 @@ class CDBReceiver():
     def __init__(self, cdb: CentralDataBus) -> None:
         self.cdb = cdb
         self.variables_to_get = []
+    
+    def wait_for_variable(self, var_name) -> None:
+        if var_name not in self.variables_to_get:
+            self.variables_to_get.append(var_name)
     
     def fetch_from_cdb(self) -> list[NameAndValue]:
         contents = self.cdb.read()
