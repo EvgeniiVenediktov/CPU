ea570586636a25adc1a1805b813218324e192a88
tyier226 <tyier226@hotmail.com>
2023-11-16 14:40:27 -0500

    updated output
    v1.0

---
+++ output.py
@@ -0,0 +1,58 @@
+class Monitor():
+    def __init__(self) -> None:
+        self.instlist = []
+
+    def output(self):
+        sorted_instrs = sorted(self.instlist, key= lambda instructions: instructions.id)
+        with open('output.txt', 'w') as file:
+            file.write("--Id--|--Issue--|--Exe--|--Mem--|--Wb--|--Commit--\n")
+            for instr in sorted_instrs:
+                file.write('{:^5} | {:^6} | {:^6} | {:^6} | {:^6} | {:^6}'.format(instr.id, instr.issue, instr.ex, instr.mem, instr.wb, instr.commit))
+                #file.write("%s\n" % sorted_instrs[x])
+        file.close()
+
+    def mark_issue(self, id, issue_cycle):
+        instr = MonitoredInstruction(id,issue_cycle)
+        self.instlist.append(instr)
+
+    def mark_ex(self, id, exec_cycle):
+        for inst in self.instlist:
+            if inst.id == id:
+                inst.ex = exec_cycle
+    
+    def mark_mem(self, id, mem_cycle):
+        for inst in self.instlist:
+            if inst.id == id:
+                inst.mem = mem_cycle
+
+    def mark_wb(self, id, wb_cycle):
+        for inst in self.instlist:
+            if inst.id == id:
+                inst.wb = wb_cycle
+
+    def mark_commit(self, id, commit_cycle):
+        for inst in self.instlist:
+            if inst.id == id:
+                inst.commit = commit_cycle
+
+class MonitoredInstruction():
+    def __init__(self,id,issue) -> None:
+        self.id = id
+        self.issue = issue
+        self.ex = 0
+        self.mem = 0
+        self.wb = 0
+        self.commit = 0
+    
+    def __str__(self) -> str:
+        strIns = f'{self.id} | {self.issue} |{self.ex} |{self.mem} |{self.wb} |{self.commit}'
+        return(strIns)
+    
+monit = Monitor()
+monit.mark_issue(0,1)
+monit.mark_ex(0,10)
+monit.mark_mem(0,11)
+monit.mark_wb(0,12)
+monit.mark_commit(0,22)
+monit.output()
+
