57e7e0ea67cb255481e6d7bf5d62fc5526125f31
tyier226 <tyier226@hotmail.com>
2023-11-17 04:41:21 -0500

    output update

--- output.py
+++ output.py
@@ -6,10 +6,10 @@ class Monitor():
         """Writes an output timetable to output.txt"""
         sorted_instrs = sorted(self.instlist, key= lambda instructions: instructions.id)
         with open('output.txt', 'w') as file:
-            file.write("|--Id--|--Issue--|-----Exe-----|--Mem--|--Wrbk--|--Commit--|\n")
+            file.write("|--Id--|--Issue--|-----Exe-----|-----Mem-----|--Wrbk--|--Commit--|\n")
             for instr in sorted_instrs:
-                file.write('|{:^5} | {:^7} | {:^5}-{:^5} | {:^5} | {:^6} | {:^9}|\n'.format(
-                    instr.id, instr.issue, instr.ex_start, instr.ex_end, instr.mem, instr.wb, instr.commit))
+                file.write('|{:^5} | {:^7} | {:^5}-{:^5} | {:^5}-{:^5} | {:^6} | {:^9}|\n'.format(
+                    instr.id, instr.issue, instr.ex_start, instr.ex_end, instr.mem_start,instr.mem_end, instr.wb, instr.commit))
         file.close()
 
     def mark_issue(self, id, issue_cycle):
@@ -26,10 +26,15 @@ class Monitor():
             if inst.id == id:
                 inst.ex_end = exec_cycle
     
-    def mark_mem(self, id, mem_cycle):
+    def mark_mem_start(self, id, mem_cycle):
         for inst in self.instlist:
             if inst.id == id:
-                inst.mem = mem_cycle
+                inst.mem_start = mem_cycle
+
+    def mark_mem_end(self, id, mem_cycle):
+        for inst in self.instlist:
+            if inst.id == id:
+                inst.mem_end = mem_cycle
 
     def mark_wb(self, id, wb_cycle):
         for inst in self.instlist:
@@ -47,7 +52,8 @@ class MonitoredInstruction():
         self.issue = issue
         self.ex_start = 0
         self.ex_end = 0
-        self.mem = 0
+        self.mem_start = 0
+        self.mem_end = 0
         self.wb = 0
         self.commit = 0
     
@@ -57,23 +63,27 @@ class MonitoredInstruction():
 
 
 if __name__ == "__main__":
+    
     monit = Monitor()
     monit.mark_issue(0,1)
     monit.mark_ex_start(0,10-5)
     monit.mark_ex_end(0,10)
-    monit.mark_mem(0,11)
+    monit.mark_mem_start(0,11)
+    monit.mark_mem_end(0,14)
     monit.mark_wb(0,12)
     monit.mark_commit(0,22)
     monit.mark_issue(1,20)
     monit.mark_ex_start(1,21-5)
     monit.mark_ex_end(1,21)
-    monit.mark_mem(1,22)
+    monit.mark_mem_start(1,22)
+    monit.mark_mem_end(1,24)
     monit.mark_wb(1,32)
     monit.mark_commit(1,34)
     monit.mark_issue(2,30)
     monit.mark_ex_start(2,31-5)
     monit.mark_ex_end(2,31)
-    monit.mark_mem(2,32)
+    monit.mark_mem_start(2,32)
+    monit.mark_mem_end(2,33)
     monit.mark_issue(3,59)
     monit.mark_wb(3,99)
     monit.mark_wb(2,98)
